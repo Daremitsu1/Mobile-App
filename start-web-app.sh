091161d9401416f02f2aeb25239d659bf9ffd575
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-01-23 00:54:07 +0530

    Update and rename web-app.sh to start-web-app.sh

--- start-web-app.sh
+++ start-web-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the web app container
-docker start traffic-congestion-web-app
+docker run -d -p 3000:3000 -p 8000:8000 --name traffic-congestion-app myimage
 
 # Schedule the container to stop after 8 hours
 (sleep 28800; docker stop traffic-congestion-web-app) &
