6517598b04e34846dbc3c99d1a2b1fb6f9c6fafa
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-01-23 00:34:32 +0530

    Update web-app.py

--- web-app.py
+++ web-app.py
@@ -1,15 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import pickle
+import os
 
 app = Flask(__name__)
 
 # load the pre-trained model
 model = pickle.load(open("traffic_congestion_model.pkl", "rb"))
 
+# define a route for the home page
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+# define a route for the prediction
 @app.route('/predict', methods=['POST'])
 def predict():
     # get the data from the request
-    data = request.get_json()
+    data = request.form
 
     # check for missing data
     if 'traffic_volume' not in data or 'weather_condition' not in data or 'time_of_day' not in data or 'day_of_week' not in data:
@@ -21,9 +28,20 @@ def predict():
     # return the prediction as a response
     return jsonify({'prediction': prediction[0]})
 
+# define a route for the about page
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# handle file not found error
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({'error': 'Not found'}), 404
+    return render_template("404.html"), 404
+
+# handle internal server errors
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("500.html"), 500
 
 if __name__ == '__main__':
     app.run(port=8000)
