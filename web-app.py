7d21fd8ee34ff80e49c616a60e4354424a3b5af2
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-01-23 00:32:28 +0530

    Update and rename app.py to web-app.py

--- web-app.py
+++ web-app.py
@@ -10,12 +10,20 @@ model = pickle.load(open("traffic_congestion_model.pkl", "rb"))
 def predict():
     # get the data from the request
     data = request.get_json()
-   
+
+    # check for missing data
+    if 'traffic_volume' not in data or 'weather_condition' not in data or 'time_of_day' not in data or 'day_of_week' not in data:
+        return jsonify({'error': 'Missing data'}), 400
+
     # make a prediction
     prediction = model.predict([[data['traffic_volume'], data['weather_condition'], data['time_of_day'], data['day_of_week']]])
-    
+
     # return the prediction as a response
     return jsonify({'prediction': prediction[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == '__main__':
     app.run(port=8000)
